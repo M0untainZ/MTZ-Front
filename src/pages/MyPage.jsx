eb681cb24c3273d826c21a8f7fba80b48a433e9f
OH <rudals2148@naver.com>
2022-11-07 14:42:52 +0900

    🐛Fix : Add Line

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from "styled-components";
+// import Modal from "../common/modal/Modal";
 
 const MyPage = () => {
+    // const [modalOn, setModalOn] = useState(false);
+    // const onModalOpen = () => {
+    //     setModalOn(true)
+    // }
     return (
-        <h1>마이페이지</h1>
+        <></>
+        // <StWrap>
+        //     <button onClick={onModalOpen}>모달열기</button>
+        //     {modalOn && (<Modal open={modalOn} onClose={() => {setModalOn(false)}}>
+                
+        //     </Modal>)}
+        // </StWrap>
     );
 }
 
 export default MyPage;
+
+// const StWrap = styled.div`
+//     width: 1000px;
+//     height: 1000px;
+// `;

a930285ea16f60916e5eb459ecfa2ac6bee73e3f
kimminseok <kms3288381@naver.com>
2022-11-08 21:57:28 +0900

    ⭐Feat : Implement login, signup

--- src/pages/DetailTwo.jsx
+++ src/pages/DetailTwo.jsx
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../common/modal/Modal";
+import styled from "styled-components";
 
 const DetailTwo = () => {
-    return (
-        <h1>상세페이지 2</h1>
-    )
-}
+  const [modal, setModal] = useState(false);
+  const ModalSwitch = () => {
+    setModal(!modal);
+  };
+  return (
+    <StContainer>
+      <div>
+        <div>지도</div>
+      </div>
+      <div>
+        <div>산 정보 / 난이도 / 시간</div>
+        <span>
+          <button>좋아요</button>
+        </span>
+      </div>
+      <div>
+        <img></img>
+        <img></img>
+        <img></img>
+        <img></img>
+        <img></img>
+        <img></img>
+        <button>+99</button>
+      </div>
+      <div>
+        <button onClick={ModalSwitch}>인증하기</button>
+        {modal && (
+          <Modal
+            open={modal}
+            onClose={() => {
+              setModal(false);
+            }}
+          >
+            <StModalBox>
+              <div className="titleBox">
+                <div>등산 갔음???</div>
+              </div>
+              <div className="imgBox">
+                인증 사진
+                <img></img>
+              </div>
+              <div className="addBox">
+                <button>사진 추가하기</button>
+              </div>
+              <div className="buttonBox">
+                <button
+                  className="cancelButton"
+                  onClick={() => {
+                    setModal(false);
+                  }}
+                >
+                  취소
+                </button>
+                <button className="addButton">등록</button>
+              </div>
+            </StModalBox>
+          </Modal>
+        )}
+      </div>
+    </StContainer>
+  );
+};
 
 export default DetailTwo;
+
+const StContainer = styled.div`
+  text-align: center;
+`;
+
+const StModalBox = styled.div`
+  text-align: center;
+  padding-top: 3%;
+  .titleBox {
+    margin-bottom: 5%;
+    font-size: xx-large;
+  }
+  .imgBox {
+    height: 25vh;
+    border: 1px solid;
+  }
+  .addBox {
+    button {
+      width: 100%;
+      margin-top: 5%;
+      border: 0.5px solid;
+    }
+  }
+  .buttonBox {
+    width: 40%;
+    margin-left: 60%;
+    margin-top: 10%;
+    .cancelButton {
+      width: 40%;
+      border: 0.5px solid;
+    }
+    .addButton {
+      margin-left: 15%;
+      width: 40%;
+      border: 0.5px solid;
+    }
+  }
+`;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { __addUsers } from "../redux/modules/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  __addUsers,
+  overlapEmailCheck,
+  __nameCheck,
+  __emailCheck,
+} from "../redux/modules/userSlice";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -16,6 +21,16 @@ const SignUp = () => {
     region: "",
   };
   const [user, setUser] = useState(initialState);
+  const { overlapEmail, overlapName } = useSelector((state) => state.user);
+  const [emailCheck, setEmailCheck] = useState(false);
+  const [pwCheck, setPwCheck] = useState(false);
+  const [emailState, setEmailState] = useState(false);
+
+  const regEmail =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
+
+  const regPw =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
@@ -23,8 +38,39 @@ const SignUp = () => {
       ...user,
       [name]: value,
     });
-    console.log(user);
   };
+
+  //이메일 중복검사
+  const EmailCk = () => {
+    if (emailCheck === true) {
+      setEmailState(true);
+      dispatch(__emailCheck({ email: user.email }));
+    }
+  };
+
+  //닉네임 중복검사
+  const NameCk = () => {
+    dispatch(__nameCheck({ nickName: user.nickName }));
+  };
+
+  useEffect(() => {
+    if (regEmail.test(user.email)) {
+      setEmailCheck(true);
+    } else {
+      setEmailCheck(false);
+    }
+    dispatch(overlapEmailCheck());
+    // eslint-disable-next-line
+  }, [user.email]);
+
+  useEffect(() => {
+    if (regPw.test(user.password)) {
+      setPwCheck(true);
+    } else {
+      setPwCheck(false);
+    }
+    // eslint-disable-next-line
+  }, [user.password]);
 
   const onSubmit = () => {
     dispatch(
@@ -33,11 +79,12 @@ const SignUp = () => {
         nickName: user.nickName,
         password: user.password,
         passwordConfirm: user.passwordConfirm,
-        region: user.region,
+        memberRegion: user.region,
       })
     );
     setUser(initialState);
-    // navigate("/login")
+    alert("회원가입이 완료되었습니다!");
+    navigate("/login");
   };
 
   return (
@@ -49,16 +96,38 @@ const SignUp = () => {
         <StInputBox>
           <div className="emailInputBox">
             <input
+              onBlur={EmailCk}
               onChange={onChangeHandler}
               name="email"
               value={user.email}
+              autoComplete="off"
               className="emailInput"
               type="text"
               placeholder="이메일"
             />
+            <div>
+              {user.email.trim() === "" ? null : emailState ? (
+                overlapEmail ? (
+                  <div style={{ color: "blue" }}>사용 가능한 이메일입니다.</div>
+                ) : (
+                  <StErrorMassage>
+                    <StDangerImg src="https://member.op.gg/icon_alert.953d9b77.svg" />
+                    이미 사용중인 이메일 입니다.
+                  </StErrorMassage>
+                )
+              ) : emailCheck ? (
+                <div style={{ color: "blue" }}>형식에 맞는 이메일 입니다.</div>
+              ) : (
+                <StErrorMassage>
+                  <StDangerImg src="https://member.op.gg/icon_alert.953d9b77.svg" />
+                  이메일이 형식에 맞지 않습니다.
+                </StErrorMassage>
+              )}
+            </div>
           </div>
           <div className="nickNameInputBox">
             <input
+              onBlur={NameCk}
               onChange={onChangeHandler}
               name="nickName"
               value={user.nickName}
@@ -66,6 +135,14 @@ const SignUp = () => {
               type="text"
               placeholder="닉네임"
             />
+            {user.nickName.trim() === "" ? null : overlapName ? (
+              <div style={{ color: "blue" }}>사용 할 수 있는 닉네임입니다.</div>
+            ) : (
+              <StErrorMassage>
+                <StDangerImg src="https://member.op.gg/icon_alert.953d9b77.svg" />
+                이미 사용중인 닉네임입니다.
+              </StErrorMassage>
+            )}
           </div>
           <div className="pwInputBox">
             <input
@@ -76,6 +153,16 @@ const SignUp = () => {
               type="password"
               placeholder="비밀번호"
             />
+            <div>
+              {user.password.trim() === "" ? null : pwCheck ? (
+                <div style={{ color: "blue" }}>올바른 비밀번호입니다.</div>
+              ) : (
+                <StErrorMassage>
+                  <StDangerImg src="https://member.op.gg/icon_alert.953d9b77.svg" />
+                  비밀번호가 형식에 맞지 않습니다.
+                </StErrorMassage>
+              )}
+            </div>
           </div>
           <div className="pwCheckBox">
             <input
@@ -86,6 +173,17 @@ const SignUp = () => {
               type="password"
               placeholder="비밀번호 확인"
             />
+            <div>
+              {user.password.trim() === "" ? null : user.password ===
+                user.passwordConfirm ? (
+                <div style={{ color: "blue" }}>비밀번호가 일치합니다.</div>
+              ) : (
+                <StErrorMassage>
+                  <StDangerImg src="https://member.op.gg/icon_alert.953d9b77.svg" />
+                  비밀번호가 일치하지 않습니다.
+                </StErrorMassage>
+              )}
+            </div>
           </div>
           <StSelectBox>
             <div className="selectName">
@@ -108,9 +206,15 @@ const SignUp = () => {
         </StInputBox>
 
         <StKakaOButton>
-          <button className="kakaO" onClick={onSubmit}>
-            가입하기
-          </button>
+          {emailCheck && overlapEmail && overlapName && pwCheck ? (
+            <button className="kakaO" onClick={onSubmit}>
+              가입하기
+            </button>
+          ) : (
+            <button className="kakaO" disabled="disabled">
+              가입하기
+            </button>
+          )}
         </StKakaOButton>
       </StLoginBox>
     </StLoginContainer>
@@ -210,4 +314,13 @@ const StSelectBox = styled.div`
       width: 100%;
     }
   }
+`;
+
+const StErrorMassage = styled.div`
+  color: red;
+  margin-top: 5px;
+`;
+const StDangerImg = styled.img`
+  width: 14px;
+  margin-right: 5px;
 `;

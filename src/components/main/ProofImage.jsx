e3fdaccc8dd795f5529ce7c73711ad1e4649bfa7
hyunjin lee <limgx5192@gmail.com>
2022-11-09 21:05:27 +0900

    ⭐Feat : add header feature / 🎨Design : main

--- src/components/main/ProofImage.jsx
+++ src/components/main/ProofImage.jsx
@@ -1,67 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const ProofImage = () => {
-    return (
-        <>
-            <StProofWrap>
-                <p>인증사진 📷</p>
-                <StImageList>
-                    <button className="prev">⬅</button>
-                    <button className="next">➡</button>
-                    <img src="/icons/picture.jpg" />
-                    <img src="/icons/pic.PNG" />
-                    <img src="/icons/img.jpg" />
-                    <img src="/icons/img1.jpg" />
-                    <img src="/icons/mt.jpg" />
-                    <img src="/icons/mt2.jpg" />
-                    <img src="/icons/mt3.jpg" />
-                </StImageList>
-            </StProofWrap>
-        </>
-    );
-}
+     return (
+          <>
+               <StProofWrap>
+                    <p>인증사진 📷</p>
+                    <StImageList>
+                         <button className="img-btn prev">
+                              <IoIosArrowBack />
+                         </button>
+                         <button className="img-btn next">
+                              <IoIosArrowForward />
+                         </button>
+                         <img src="/icons/picture.jpg" alt="" />
+                         <img src="/icons/pic.PNG" alt="" />
+                         <img src="/icons/img.jpg" alt="" />
+                         <img src="/icons/img1.jpg" alt="" />
+                         <img src="/icons/mt.jpg" alt="" />
+                         <img src="/icons/mt2.jpg" alt="" />
+                         <img src="/icons/mt3.jpg" alt="" />
+                    </StImageList>
+               </StProofWrap>
+          </>
+     );
+};
 
 export default ProofImage;
 
 const StProofWrap = styled.div`
-    width: 70%;
-    height: 20%;
-    margin: 20px auto 30px;
-    p {
-        margin-bottom: 10px;
-    }
+     width: 70%;
+     height: 20%;
+     margin: 20px auto;
+     box-sizing: border-box;
+     position: relative;
+     p {
+          margin-bottom: 10px;
+     }
 `;
 
 const StImageList = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
-    gap: 14px;
-    align-items: center;
-    overflow: hidden;
-    position: relative;
-    border: 1px solid black;
-    img {
-        width: 235px;
-        height: 260px;
-        object-fit: cover;
-    }
-    button {
-        position: absolute;
-        font-weight: bolder;
-        width: 30px;
-        height: 30px;
-        border: none;
-        border-radius: 50%;
-        background-color: rgba(0, 0, 0, 0.5);
-        color: #fff;
-    }
-    .prev {
-        left: 5px;
-    }
-    .next {
-        right: 5px;
-    }
+     width: 100%;
+     height: 100%;
+     display: flex;
+     justify-content: space-between;
+     gap: 14px;
+     align-items: center;
+     overflow: hidden;
+     /* border: 1px solid black; */
+     img {
+          width: 235px;
+          height: 260px;
+          object-fit: cover;
+     }
+     .img-btn {
+          font-weight: bolder;
+          width: 30px;
+          height: 30px;
+          border: none;
+          border-radius: 50%;
+          background-color: transparent;
+          font-size: 25px;
+          /* background-color: rgba(0, 0, 0, 0.5); */
+          /* color: #fff; */
+     }
+     .prev {
+          left: -5%;
+          position: absolute;
+     }
+     .next {
+          right: -5%;
+          position: absolute;
+     }
 `;
--- src/components/main/UserRanking.jsx
+++ src/components/main/UserRanking.jsx
@@ -2,84 +2,93 @@ import React from "react";
 import styled from "styled-components";
 
 const UserRanking = () => {
-    return (
-        <>
-            <StRankingWrap>
-                <p>최고의 MTZ 프렌즈를 만나보세요 !</p>
-                <StUserRank>
-                    <StUserInfo>
-                        <span className="rank">1</span>
-                        <img src="logo192.png" />
-                        <p className="user-name">나는야셰르파</p>
-                        <span className="badge-cnt">모은 뱃지 갯수 50개</span>
-                    </StUserInfo>
-                    <StUserInfo>
-                        <span className="rank">2</span>
-                        <img src="logo192.png" />
-                        <p className="user-name">나는야셰르파</p>
-                        <span className="badge-cnt">모은 뱃지 갯수 50개</span>
-                    </StUserInfo>
-                    <StUserInfo>
-                        <span className="rank">3</span>
-                        <img src="logo192.png" />
-                        <p className="user-name">나는야셰르파</p>
-                        <span className="badge-cnt">모은 뱃지 갯수 50개</span>
-                    </StUserInfo>
-                </StUserRank>         
-            </StRankingWrap>
-        </>
-    );
-}
+     return (
+          <>
+               <StRankingWrap>
+                    <p>최고의 MTZ 프렌즈를 만나보세요 !</p>
+                    <StUserRank>
+                         <StUserInfo>
+                              <span className="rank">1</span>
+                              <img src="logo192.png" />
+                              <p className="user-name">나는야셰르파</p>
+                              <span className="badge-cnt">
+                                   모은 뱃지 갯수 50개
+                              </span>
+                         </StUserInfo>
+                         <StUserInfo>
+                              <span className="rank">2</span>
+                              <img src="logo192.png" />
+                              <p className="user-name">나는야셰르파</p>
+                              <span className="badge-cnt">
+                                   모은 뱃지 갯수 50개
+                              </span>
+                         </StUserInfo>
+                         <StUserInfo>
+                              <span className="rank">3</span>
+                              <img src="logo192.png" />
+                              <p className="user-name">나는야셰르파</p>
+                              <span className="badge-cnt">
+                                   모은 뱃지 갯수 50개
+                              </span>
+                         </StUserInfo>
+                    </StUserRank>
+               </StRankingWrap>
+          </>
+     );
+};
 
 export default UserRanking;
 
 const StRankingWrap = styled.div`
-  width: 70%;
-  height: 25vh;
-  margin: 30px auto 30px;
+     width: 70%;
+     height: 25vh;
+     margin: 30px auto 30px;
+     p {
+          font-weight: bold;
+     }
 `;
 
 const StUserRank = styled.div`
-  width: 100%;
-  height: 90%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+     width: 100%;
+     height: 90%;
+     display: flex;
+     align-items: center;
+     justify-content: space-between;
 `;
 
 const StUserInfo = styled.div`
-    width: 33%;
-    height: 100%;
-    background-color: #fff;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    position: relative;
-    margin-top: 30px;
-    img {
-        width: 80px;
-        height: 80px;
-        border: 1px solid black;
-        border-radius: 30px;
-        margin-top: 10%;
-    }
-    .rank {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        margin: 30px 70px 0px 0px;
-        width: 20px;
-        height: 20px;
-        border: 1px solid black;
-        border-radius: 50%;
-        background-color: #fff;
-    }
-    .user-name {
-        margin-top: 15px;
-    }
-    .badge-cnt {
-        font-size: 14px;
-        margin-top: 10px;
-    }
+     width: 33%;
+     height: 100%;
+     background-color: #fff;
+     display: flex;
+     align-items: center;
+     flex-direction: column;
+     position: relative;
+     margin-top: 30px;
+     img {
+          width: 80px;
+          height: 80px;
+          border: 1px solid black;
+          border-radius: 30px;
+          margin-top: 10%;
+     }
+     .rank {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          margin: 30px 70px 0px 0px;
+          width: 20px;
+          height: 20px;
+          border: 1px solid black;
+          border-radius: 50%;
+          background-color: #fff;
+     }
+     .user-name {
+          margin-top: 15px;
+     }
+     .badge-cnt {
+          font-size: 14px;
+          margin-top: 10px;
+     }
 `;

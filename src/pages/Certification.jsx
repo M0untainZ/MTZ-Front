83f0f8688375e087dc68e3cf92b7a9aa7522141d
OH <rudals2148@naver.com>
2022-11-13 20:01:11 +0900

    🤝Test : code test

--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import styled from "styled-components";
 
 const Certification = () => {
      return (
-          <>
-               <h1>인증페이지</h1>
-          </>
+          <StContainer>
+               <StProofContainer>
+                    <StProofImg>
+                         <img src="" alt="" />
+                    </StProofImg>
+               </StProofContainer>
+               <StProofContainer>
+                    <StProofImg>
+                         <img src="" alt="" />
+                    </StProofImg>
+               </StProofContainer>
+               <StProofContainer>
+                    <StProofImg>
+                         <img src="" alt="" />
+                    </StProofImg>
+               </StProofContainer>
+               <StProofContainer>
+                    <StProofImg>
+                         <img src="" alt="" />
+                    </StProofImg>
+               </StProofContainer>
+          </StContainer>
      );
 };
 
 export default Certification;
+
+const StContainer = styled.div`
+     width: 100%;
+     height: 100%;
+     display: flex;
+     gap: 55px;
+`;
+
+const StProofContainer = styled.div`
+     width: 15vw;
+     height: 35vh;
+     border: 1px solid black;
+     padding: 10px;
+`;
+
+const StProofImg = styled.div`
+     width: 100%;
+     height: 90%;
+     border: 1px solid red;
+     img {
+          width: 100%;
+          height: 100%;
+     }
+`;

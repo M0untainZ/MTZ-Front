cc4210af33f5ac5fc878d0468d6ccb9beeb5da50
OH <rudals2148@naver.com>
2022-11-14 23:05:00 +0900

    🔨Refactor : code refactoring

--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -1,55 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-
 const Certification = () => {
      return (
-          <StContainer>
-               <StProofContainer>
-                    <StProofImg>
-                         <img src="" alt="" />
-                    </StProofImg>
-               </StProofContainer>
-               <StProofContainer>
-                    <StProofImg>
-                         <img src="" alt="" />
-                    </StProofImg>
-               </StProofContainer>
-               <StProofContainer>
-                    <StProofImg>
-                         <img src="" alt="" />
-                    </StProofImg>
-               </StProofContainer>
-               <StProofContainer>
-                    <StProofImg>
-                         <img src="" alt="" />
-                    </StProofImg>
-               </StProofContainer>
-          </StContainer>
+          <></>
      );
 };
 
 export default Certification;
-
-const StContainer = styled.div`
-     width: 100%;
-     height: 100%;
-     display: flex;
-     gap: 55px;
-`;
-
-const StProofContainer = styled.div`
-     width: 15vw;
-     height: 35vh;
-     border: 1px solid black;
-     padding: 10px;
-`;
-
-const StProofImg = styled.div`
-     width: 100%;
-     height: 90%;
-     border: 1px solid red;
-     img {
-          width: 100%;
-          height: 100%;
-     }
-`;

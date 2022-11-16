8f7f9846d4789fe8fd175831f90d09e3cccb401e
OH <rudals2148@naver.com>
2022-11-16 23:55:03 +0900

    🎨Design: ui

---
+++ src/components/certification/ProofList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import FilterList from "./FilterList";
+import Proof from "./Proof";
+
+const ProofList = () => {
+    return (
+        <StProof>
+            <FilterList />
+            <Proof />
+        </StProof>
+    );
+}
+
+export default ProofList;
+
+const StProof = styled.div`
+    width: 1400px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid black;
+`;
--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import ProofList from "../components/certification/ProofList";
+
 const Certification = () => {
      return (
-          <></>
+          <StCertification>
+               <ProofList />
+          </StCertification>
      );
 };
 
 export default Certification;
+
+const StCertification = styled.div`
+     display: flex;
+     justify-content: center;
+`;

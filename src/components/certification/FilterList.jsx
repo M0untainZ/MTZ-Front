4021a711fd463cf3bbb50b6c743d675f9a877e85
OH <rudals2148@naver.com>
2022-11-17 23:52:09 +0900

    🔨Refactor : code refactoring

--- src/components/certification/FilterList.jsx
+++ src/components/certification/FilterList.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import styled from "styled-components";
 
 const FilterList = () => {
+    const [checkValue, setCheckValue] = useState("");
     const regionList = [
         { id: 0, value: "서울" },
         { id: 1, value: "강원" },
@@ -13,21 +14,24 @@ const FilterList = () => {
     ];
 
     const mtList = {
-        seoul: ["관악산", "북한산", "아차산", "도봉산", "수락산", "인왕산", "북악산", "남산"],
-        gangwon: ["설악산", "치악산", "태백산", "오대산"],
-        gyeonggi: ["명성산", "백운산", "마니산", "감악산", "소요산", "운악산", "명지산", "축령산", "유명산", "용문산"],
-        chungchung: ["속리산", "월악산", "소백산", "계룡산"],
-        gyeongsang: ["팔공산", "대야산", "비슬산", "덕유산", "금정산"],
-        jeolla: ["지리산", "대둔산", "조계산"],
-        jeju: ["한라산"]
+        "서울": ["관악산", "북한산", "아차산", "도봉산", "수락산", "인왕산", "북악산", "남산"],
+        "강원": ["설악산", "치악산", "태백산", "오대산"],
+        "경기": ["명성산", "백운산", "마니산", "감악산", "소요산", "운악산", "명지산", "축령산", "유명산", "용문산"],
+        "충청": ["속리산", "월악산", "소백산", "계룡산"],
+        "경상": ["팔공산", "대야산", "비슬산", "덕유산", "금정산"],
+        "전라": ["지리산", "대둔산", "조계산"],
+        "제주": ["한라산"]
     };
     //체크박스 하나만 선택되게 하기
-    const onIsCheck = (isChecked) => {
-        const checkBox = document.getElementsByClassName("region");
-        for (let i = 0; i < checkBox.length; i++) {
-            if (checkBox[i] !== isChecked) checkBox[i].checked = false;
-        }
-    };
+    // const onIsCheck = (isChecked) => {
+    //     const checkBox = document.getElementsByClassName("region");
+    //     for (let i = 0; i < checkBox.length; i++) {
+    //         if (checkBox[i] !== isChecked) checkBox[i].checked = false;
+    //     }
+    // };
+    const onCheckHanlder = (radioBtnValue) => {
+        setCheckValue(radioBtnValue);
+    }
 
     return (
         <StFilterContainer>
@@ -42,7 +46,13 @@ const FilterList = () => {
             <div className="region-list">
                 {regionList.map((el) => (
                     <label key={el.id}>
-                        <StyledInput type="radio" name="region" className="region" value={el.value} onChange={() => onIsCheck} />
+                        <StyledInput 
+                            type="radio" 
+                            name="region" 
+                            className="region" 
+                            checked={checkValue === el.value}
+                            onChange={() => onCheckHanlder(el.value)} 
+                        />
                         {el.value}
                     </label>
                 ))}    

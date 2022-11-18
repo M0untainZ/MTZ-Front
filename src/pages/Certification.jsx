d6dcf5f5faa5348d3e30a9b3bce6053437752728
OH <rudals2148@naver.com>
2022-11-18 23:41:54 +0900

    ⭐Feat : go-top btn

--- src/pages/Certification.jsx
+++ src/pages/Certification.jsx
@@ -3,8 +3,17 @@ import styled from "styled-components";
 import ProofList from "../components/certification/ProofList";
 
 const Certification = () => {
+
+     const onTopHandler = () => {
+          window.scrollTo({
+               top: 0,
+               behavior: "smooth"
+          });
+     };
+
      return (
           <StCertification>
+               <button className="go-top" onClick={onTopHandler}></button>
                <ProofList />
           </StCertification>
      );
@@ -15,4 +24,15 @@ export default Certification;
 const StCertification = styled.div`
      display: flex;
      justify-content: center;
+     position: relative;
+     .go-top {
+        width: 30px;
+        height: 30px;
+        border: 1px solid purple;
+        border-radius: 50%;
+        position: fixed;
+        bottom: 5%;
+        right: 6%;
+        cursor: pointer;
+    }
 `;

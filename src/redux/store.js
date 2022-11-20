8403e1344e0cb42eb89cf73f6ab32c2a2df31987
OH <rudals2148@naver.com>
2022-11-20 23:28:38 +0900

    ⭐Feat : proof

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import user from "../redux/modules/userSlice";
 import mountains from "../redux/modules/mountainsSlice";
 import twoSlice from "./modules/twoSlice";
+import proofs from "./modules/proofSlice";
+
 const reducer = combineReducers({
      user,
      mountains,
      twoSlice,
+     proofs
 });
 
 export default configureStore({

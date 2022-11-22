c80c28cfd1b2f56c2104f38c32223c032e9f6877
kimminseok <kms3288381@naver.com>
2022-11-22 22:39:23 +0900

    ⭐Feat : detailtwo page add img swiper

--- src/components/detailTwo/MountPhoto.jsx
+++ src/components/detailTwo/MountPhoto.jsx
@@ -1,38 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { useSelector } from "react-redux";
 import ImgModal from "./imgModal/imgModal";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import SwiperCore, { Navigation } from "swiper/core";
+import PhotoModal from "./photoModal";
 const MountPhoto = () => {
   const [modal, setModal] = useState(false);
+
   const ModalSwitch = () => {
     setModal(!modal);
   };
-
   const photoList = useSelector(
     (state) => state.twoSlice.mountain.data?.certificatedMountainList
   );
+
+  SwiperCore.use([Navigation]);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  const [swiperOptions, setSwiperOptions] = useState(null);
+
+  useEffect(() => {
+    if (!swiperOptions) {
+      // swiper 컴포넌트에 옵션을 직접 넣으면 코드가 길어지기 때문에 따로 빼서 사용
+      const options = {
+        spaceBetween: 30,
+        navigation: {
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        },
+        slidesPerView: 7,
+        onBeforeInit: (swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.update();
+        },
+      };
+      setSwiperOptions(options);
+    }
+  }, [swiperOptions]);
 
   return (
     <>
       <StContainer>
         <p>수락산을 등반한 MTZ들의 인증사진</p>
+
         <StImageList>
-          <button className="img-btn prev">
+          <PrevBtn ref={prevRef} className="disabled">
             <IoIosArrowBack />
-          </button>
-          <button className="img-btn next">
+          </PrevBtn>
+          {swiperOptions && (
+            <Swiper {...swiperOptions} className="swiper">
+              {photoList?.map((photoList, index) => (
+                <SwiperSlide className="swiper-slide" key={index}>
+                  <img
+                    src={`${photoList.photo}`}
+                    alt=""
+                    onClick={ModalSwitch}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+          <NextBtn className="img-btn next">
             <IoIosArrowForward />
-          </button>
-          {photoList?.map((photoList, index) => (
-            <img
-              key={index}
-              src={`${photoList.photo}`}
-              alt=""
-              onClick={ModalSwitch}
-            />
-          ))}
+          </NextBtn>
         </StImageList>
+
         {modal && (
           <ImgModal
             open={modal}
@@ -40,11 +76,7 @@ const MountPhoto = () => {
               setModal(false);
             }}
           >
-            <StModalBox>
-              {photoList.map((photoList, index) => (
-                <img key={index} src={`${photoList.photo}`} alt="" />
-              ))}
-            </StModalBox>
+            <PhotoModal />
           </ImgModal>
         )}
       </StContainer>
@@ -70,37 +102,63 @@ const StImageList = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: space-between;
   gap: 14px;
   align-items: center;
-  overflow: hidden;
-  /* border: 1px solid black; */
-  img {
-    width: 15vh;
+  .swiper {
+    width: 100%;
     height: 15vh;
-    object-fit: cover;
+    overflow: hidden;
+    &-button-disabled {
+      visibility: hidden;
+    }
   }
-  .img-btn {
-    font-weight: bolder;
-    width: 30px;
-    height: 30px;
-    border: none;
-    border-radius: 50%;
-    background-color: transparent;
-    font-size: 25px;
-    /* background-color: rgba(0, 0, 0, 0.5); */
-    /* color: #fff; */
-  }
-  .prev {
-    left: -5%;
-    position: absolute;
-  }
-  .next {
-    right: -5%;
-    position: absolute;
+  .swiper-slide {
+    width: 300px;
+    height: 100%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 `;
 
+const PrevBtn = styled.button`
+  width: 25px;
+  height: 25px;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: black;
+  position: absolute;
+  z-index: 10;
+  left: -2vw;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0 5px black;
+  }
+`;
+const NextBtn = styled.button`
+  width: 25px;
+  height: 25px;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: black;
+  position: absolute;
+  z-index: 10;
+  right: -2vw;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0 5px black;
+  }
+`;
 const StModalBox = styled.div`
   padding-left: 10%;
   margin-top: -25px;
--- src/components/main/ProofImage.jsx
+++ src/components/main/ProofImage.jsx
@@ -8,133 +8,135 @@ import SwiperCore, { Navigation } from "swiper/core";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
 const ProofImage = () => {
-     const [mainProof, setMainProof] = useState([]);
-     const {data} = useQuery(["main"], getMain, {
-          onSuccess: (config) => setMainProof(config.data.certificationPhoto.reverse().slice(0, 10))})
+  const [mainProof, setMainProof] = useState([]);
+  const { data } = useQuery(["main"], getMain, {
+    onSuccess: (config) =>
+      setMainProof(config.data.certificationPhoto.reverse().slice(0, 10)),
+  });
 
-     SwiperCore.use([Navigation]);
-     const prevRef = useRef(null);
-     const nextRef = useRef(null);
+  SwiperCore.use([Navigation]);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
-     const [swiperOptions, setSwiperOptions] = useState(null);
+  const [swiperOptions, setSwiperOptions] = useState(null);
 
-     useEffect(() => {
-          if (!swiperOptions) {
-               // swiper 컴포넌트에 옵션을 직접 넣으면 코드가 길어지기 때문에 따로 빼서 사용
-               const options = {
-                    spaceBetween: 30,
-                    navigation: {
-                         prevEl: prevRef.current,
-                         nextEl: nextRef.current,
-                    },
-                    slidesPerView: 7,
-                    onBeforeInit: (swiper) => {
-                         swiper.params.navigation.prevEl = prevRef.current;
-                         swiper.params.navigation.nextEl = nextRef.current;
-                         swiper.navigation.update();
-                    },
-               };
-               setSwiperOptions(options);
-          }
-     }, [swiperOptions]);
+  useEffect(() => {
+    if (!swiperOptions) {
+      // swiper 컴포넌트에 옵션을 직접 넣으면 코드가 길어지기 때문에 따로 빼서 사용
+      const options = {
+        spaceBetween: 30,
+        navigation: {
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        },
+        slidesPerView: 7,
+        onBeforeInit: (swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.update();
+        },
+      };
+      setSwiperOptions(options);
+    }
+  }, [swiperOptions]);
 
-     return (
-          <>
-               <StProofWrap>
-                    <p>인증사진 📷</p>
-                    <StImageList>
-                         <PrevBtn ref={prevRef} className="disabled">
-                              <FaLongArrowAltLeft />
-                         </PrevBtn>
-                         {swiperOptions &&  (
-                              <Swiper {...swiperOptions} className="swiper">
-                                   {mainProof.map((el, idx) =>
-                                        <SwiperSlide className="swiper-slide" key={idx}>
-                                             <img src={`${el.photo}`} alt="" />
-                                        </SwiperSlide>
-                                        )}
-                              </Swiper>
-                         )}
-                         <NextBtn ref={nextRef} className="disabled">
-                              <FaLongArrowAltRight />
-                         </NextBtn>
-                    </StImageList>
-               </StProofWrap>
-          </>
-     );
+  return (
+    <>
+      <StProofWrap>
+        <p>인증사진 📷</p>
+        <StImageList>
+          <PrevBtn ref={prevRef} className="disabled">
+            <FaLongArrowAltLeft />
+          </PrevBtn>
+          {swiperOptions && (
+            <Swiper {...swiperOptions} className="swiper">
+              {mainProof.map((el, idx) => (
+                <SwiperSlide className="swiper-slide" key={idx}>
+                  <img src={`${el.photo}`} alt="" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+          <NextBtn ref={nextRef} className="disabled">
+            <FaLongArrowAltRight />
+          </NextBtn>
+        </StImageList>
+      </StProofWrap>
+    </>
+  );
 };
 
 export default ProofImage;
 
 const StProofWrap = styled.div`
-     width: 70%;
-     height: 20%;
-     margin: 30px auto;
-     box-sizing: border-box;
-     position: relative;
-     p {
-          margin-bottom: 10px;
-     }
+  width: 70%;
+  height: 20%;
+  margin: 30px auto;
+  box-sizing: border-box;
+  position: relative;
+  p {
+    margin-bottom: 10px;
+  }
 `;
 
 const StImageList = styled.div`
-     width: 100%;
-     height: 100%;
-     display: flex;
-     align-items: center;
-     .swiper {
-          width: 100%;
-          height: 15vh;
-          overflow: hidden;
-          &-button-disabled {
-               visibility: hidden;
-          }
-     }
-     .swiper-slide {
-          width: 300px;
-          height: 100%;
-          img {
-               width: 100%;
-               height: 100%;
-               object-fit: cover;
-          }
-     }
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  .swiper {
+    width: 100%;
+    height: 15vh;
+    overflow: hidden;
+    &-button-disabled {
+      visibility: hidden;
+    }
+  }
+  .swiper-slide {
+    width: 300px;
+    height: 100%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 `;
 
 const PrevBtn = styled.button`
-     width: 25px;
-     height: 25px;
-     border: 1px solid black;
-     border-radius: 50%;
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     font-size: 20px;
-     color: black;
-     position: absolute;
-     z-index: 10;
-     left: -2vw;
-     cursor: pointer;
-     &:hover {
-          box-shadow: 0 0 5px black;
-     }
+  width: 25px;
+  height: 25px;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: black;
+  position: absolute;
+  z-index: 10;
+  left: -2vw;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0 5px black;
+  }
 `;
 
 const NextBtn = styled.button`
-     width: 25px;
-     height: 25px;
-     border: 1px solid black;
-     border-radius: 50%;
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     font-size: 20px;
-     color: black;
-     position: absolute;
-     z-index: 10;
-     right: -2vw;
-     cursor: pointer;
-     &:hover {
-          box-shadow: 0 0 5px black;
-     }
+  width: 25px;
+  height: 25px;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: black;
+  position: absolute;
+  z-index: 10;
+  right: -2vw;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0 5px black;
+  }
 `;

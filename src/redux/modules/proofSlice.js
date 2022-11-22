13b5edeebda32e9a0c475456e1c89ace73194e7a
OH <rudals2148@naver.com>
2022-11-22 22:53:11 +0900

    ⭐Feat : add delete

--- src/redux/modules/proofSlice.js
+++ src/redux/modules/proofSlice.js
@@ -28,28 +28,37 @@ export const __proofFilter = createAsyncThunk("FILTER_PROOF", async (payload, th
 
 export const __proofDelete = createAsyncThunk("DELETE_PROOF", async (payload, thunkAPI) => {
     try {
-       const {data} = await axios.delete(`${process.env.REACT_APP_AXIOS_API}/api/photos/sakje`);
+        const {data} = await axios.delete(`${process.env.REACT_APP_AXIOS_API}/api/photos/sakje`, {
+        data: payload,
+        headers: {
+            Authorization: sessionStorage.getItem("Access_Token"),
+        },
+       });
        return thunkAPI.fulfillWithValue(data);
     } catch(err) {
         return thunkAPI.rejectWithValue(err);
     }
 })
 
-export const __infiniteProof = createAsyncThunk("INFINITE_SCROLL", async (page, thunkAPI) => {
-    try {
-        const res = await axios.get(`${process.env.REACT_APP_AXIOS_API}/api/photos?page=${page}`);
-        if (res.data.data.length === 0) {
-            throw res
-        }
-        return thunkAPI.fulfillWithValue(res.data.data)
-    } catch (err) {
-        return thunkAPI.rejectWithValue(err)
-    }
-})
+
+// 무한스크롤 mvp 이후,  useInfiniteQuery 사용
+// export const __infiniteProof = createAsyncThunk("INFINITE_SCROLL", async (page, thunkAPI) => {
+//     try {
+//         const res = await axios.get(`${process.env.REACT_APP_AXIOS_API}/api/photos?page=${page}`);
+//         if (res.data.data.length === 0) {
+//             throw res
+//         }
+//         return thunkAPI.fulfillWithValue(res.data.data)
+//     } catch (err) {
+//         return thunkAPI.rejectWithValue(err)
+//     }
+// })
 
 const initialState = {
     proofs: [],
     isLoading: false,
+    photo: "",
+    certificationId: "",
     filter: {
         region: "",
         name: ""
@@ -71,18 +80,20 @@ export const proofSlice = createSlice({
         [__proofFilter.rejected]: (state, action) => {
 
         },
+        // 인증게시물 삭제
         [__proofDelete.fulfilled]: (state, action) => {
-            state.proofs = state.proof.filter((proof) => proof.certificationId !== action.payload);
+            state.proofs = state.proofs.filter((proof) => proof.certificationId !== action.payload);
         },
         [__proofDelete.rejected]: (state, action) => {
             state.error = action.payload;
         },
-        [__infiniteProof.fulfilled]: (state, action) => {
-            state.proofs.push(...action.payload)
-            state.isLoading = false;
-        },
-        [__infiniteProof.rejected]: (state, action) => {
-        },
+        // 무한 스크롤 mvp 이후
+        // [__infiniteProof.fulfilled]: (state, action) => {
+        //     state.proofs.push(...action.payload)
+        //     state.isLoading = false;
+        // },
+        // [__infiniteProof.rejected]: (state, action) => {
+        // },
     },
 });
 

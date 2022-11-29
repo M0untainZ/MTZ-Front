36ecad4aae4f456e84c55a75aaef3b3e41b00473
OH <rudals2148@naver.com>
2022-11-29 22:32:37 +0900

    ⭐Feat : add api

--- src/shared/api.js
+++ src/shared/api.js
@@ -48,12 +48,34 @@ export const getProof = async () => {
     return data;
 }
 
-// export const postFilter = async (dataFilter) => {
-//     const {data} = await axiosIns.post("/api/photos/filter", dataFilter);
-//     return data;
-// }
+export const getRegionTag = async (region) => {
+  const {data} = await axiosIns.get(`/api/mountains/${region}`);
+  return data;
+}
 
-// export const deleteProof = async () => {
-//     const {data} = await axiosIns.delete("/photos/sakje");
-//     return data;
-// }
+export const getSeasonTag = async (season) => {
+  const {data} = await axiosIns.get(`/api/mountains/${season}`);
+  return data;
+}
+
+export const getTimeTag = async (time) => {
+  const {data} = await axiosIns.get(`/api/mountains/${time}`);
+  return data;
+}
+
+export const getLevelTag = async (level) => {
+  const {data} = await axiosIns.get(`/api/mountains/${level}`);
+  return data;
+}
+
+export const proofFilter = async (payload) => {
+    const {data} = await axiosIns.post("/api/photos/filter", payload);
+    return data;
+}
+
+export const deleteProof = async (payload) => {
+    const {data} = await axiosIns.delete("/api/photos/sakje", {
+      data: {certificationId:payload.id, photo:payload.photo}
+    });
+    return data;
+}

9f0a96e384278985c659840a30ea9feb2f5aefb5
OH <rudals2148@naver.com>
2022-12-02 23:04:45 +0900

    ⭐Feat : add loading

--- src/elements/Loading.jsx
+++ src/elements/Loading.jsx
@@ -24,7 +24,7 @@ const StLoading = styled.div`
     height: 40px;
     animation: spin 2s linear infinite;
     path {
-      color: var(--lightGray-color);
+      color: gray;
     }
   }
   @keyframes spin {

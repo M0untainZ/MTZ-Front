fae1da6152767f0c20e7cfda330c5ce8ad1d94e9
OH <rudals2148@naver.com>
2022-12-03 23:32:25 +0900

    ⭐Feat : infinite scroll

--- src/components/certification/Proof.jsx
+++ src/components/certification/Proof.jsx
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { Fragment } from "react";
 import styled from "styled-components";
-import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from "react-query";
-import { getProof, deleteProof } from "../../shared/api";
+import { useMutation, useQueryClient, useInfiniteQuery } from "react-query";
+import { deleteProof } from "../../shared/api";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { useInView } from "react-intersection-observer";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import axios from "axios";
+import { useInView } from "react-intersection-observer"
+import Loading from "../../elements/Loading";
+import Error from "../../elements/Error";
+import { useSelector } from "react-redux";
+
+const URI = {
+    BASE: process.env.REACT_APP_AXIOS_API
+}
+
+const infiniteProof = async (pageParam) => {
+    const res = await axios.get(`${URI.BASE}/api/photos?page=${pageParam}`, {
+        headers: {
+            Authorization: sessionStorage.getItem("Access_Token"),
+        }
+    })
+    const data = res.data;
+    return data;
+}
 
 const Proof = () => {
+    const buttonState = useSelector((state) => state.proofs.filterButton)
+    const filterData = useSelector((state) => state.proofs.filterData)
     const MySwal = withReactContent(Swal)
     const queryClient = useQueryClient();
     const userNickName = JSON.parse(sessionStorage.getItem("userinfos"));
     const authority = sessionStorage.getItem("authority");
-    
-    const { data } = useQuery(["proof"], getProof);
+
     const { mutate:deleteProofs } = useMutation(deleteProof, {
         onSuccess: () => {
             queryClient.invalidateQueries(["proof"]);
@@ -23,17 +43,16 @@ const Proof = () => {
 
     const onDeleteHandler = (id, photo) => {
         MySwal.fire({
-            title: "정말 삭제하시겠어요?",
-            text: "삭제를 하면 되돌릴 수 없어요!",
+            title: "인증사진을 삭제합니다",
+            text: "삭제를 누르시면 되돌릴 수 없어요 !",
             icon: "warning",
             showCancelButton: true,
             confirmButtonText: "네",
             cancelButtonText: '아니오',
-            background: "skyblue",
-            confirmButtonColor: "yellowgreen",
-            cancelButtonColor: "darkblue",
-            color: "blue",
-            iconColor: "red",
+            background: "#ffffff",
+            confirmButtonColor: "#185B6E",
+            cancelButtonColor: "#C6C6C6",
+            iconColor: "#F24E1E",
         }).then((result) => {
             if (result.isConfirmed) {
                 deleteProofs({id, photo})
@@ -44,35 +63,87 @@ const Proof = () => {
             } 
         })
     }
-    
-    return (
-        <StImgContainer>
-            {data?.data.slice().reverse().map((el, idx) => {
-                return (
-                    <StProofBox key={idx}>
-                        <img src={el.photo} alt="" />
-                        <StProofInfo>
-                            <div className="profile-image">
-                                <img src="/icons/icon_default-badge.png" alt="" />
-                            </div>
-                            <div className="proof-info">
-                                <p>{el.name}</p>
-                                <span>{el.nickName}</span>
-                            </div>
-                            { (userNickName.nickName === el.nickName) || (authority === "ROLE_ADMIN") ? 
-                                <div className="del-btn" onClick={() => {onDeleteHandler(el.certificationId, el.photo)}}>
-                                    <img src="/icons/icon_trash-can.png" alt="" />
-                                </div>
-                                :
-                                null
-                            }
-                            <ToastContainer />
-                        </StProofInfo>
-                    </StProofBox>      
-                )})
+
+    const { ref, inView } = useInView();
+    const { data, fetchNextPage, isFetchingNextPage, status, error } = useInfiniteQuery(
+        ["proof"], ({ pageParam = 0 }) => infiniteProof(pageParam),
+        {
+            getNextPageParam: (lastPage, allPage) => {
+                if (lastPage.data.length === 12) {
+                    return allPage.length
+                }
             }
-        </StImgContainer>
-    );
+        }
+    ) 
+    useEffect(() => {
+        if (inView) fetchNextPage();
+    }, [inView])
+
+    if (status === "loading") return <Loading />
+    if (status === "error") return <Error message={(error).message} />
+
+    if (buttonState) {
+        return (
+            <StImgContainer>
+                <Fragment>
+                        {filterData?.slice().reverse().map((el, idx) => (
+                            <StProofBox key={idx}>
+                                <img src={el.photo} alt="" />
+                                <StProofInfo>
+                                    <div className="profile-image">
+                                        <img src="/icons/icon_default-badge.png" alt="" />
+                                    </div>
+                                    <div className="proof-info">
+                                        <p>{el.name}</p>
+                                        <span>{el.nickName}</span>
+                                    </div>
+                                    { (userNickName.nickName === el.nickName) || (authority === "ROLE_ADMIN") ? 
+                                        <div className="del-btn" onClick={() => {onDeleteHandler(el.certificationId, el.photo)}}>
+                                            <img src="/icons/icon_trash-can.png" alt="" />
+                                        </div>
+                                        :
+                                        null
+                                    }
+                                    <ToastContainer />
+                                </StProofInfo>
+                            </StProofBox>
+                        ))}
+                    </Fragment>
+            </StImgContainer>
+            )
+        } else {
+            return (
+                <StImgContainer>
+                {data?.pages.map((page, i) => (
+                    <Fragment key={i}>
+                        {page.data.slice().reverse().map((el, idx) => (
+                            <StProofBox key={idx}>
+                                <img src={el.photo} alt="" />
+                                <StProofInfo>
+                                    <div className="profile-image">
+                                        <img src="/icons/icon_default-badge.png" alt="" />
+                                    </div>
+                                    <div className="proof-info">
+                                        <p>{el.name}</p>
+                                        <span>{el.nickName}</span>
+                                    </div>
+                                    { (userNickName.nickName === el.nickName) || (authority === "ROLE_ADMIN") ? 
+                                        <div className="del-btn" onClick={() => {onDeleteHandler(el.certificationId, el.photo)}}>
+                                            <img src="/icons/icon_trash-can.png" alt="" />
+                                        </div>
+                                        :
+                                        null
+                                    }
+                                    <ToastContainer />
+                                </StProofInfo>
+                            </StProofBox>
+                        ))}
+                    </Fragment>
+                ))}
+                {isFetchingNextPage ? <Loading /> : <div ref={ref}></div>}
+            </StImgContainer>      
+            )
+        }
 }
 
 export default Proof;

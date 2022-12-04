b167de613e120d726f4d1e6575c3a74222bcce38
OH <rudals2148@naver.com>
2022-12-04 23:55:30 +0900

    🔨Refactor

--- src/components/main/RecommendCourse.jsx
+++ src/components/main/RecommendCourse.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { getMain } from "../../shared/api";
+import { useQuery } from "react-query";
 
 const RecommendCourse = () => {
+     const {data} = useQuery(["main"], getMain);
      const [recommendList, setRecommendList] = useState(false);
      const onListToggle = () => {
           setRecommendList(!recommendList);
      };
+
      return (
           <>
                <StRecommendWrap>
@@ -17,69 +21,26 @@ const RecommendCourse = () => {
                               <span onClick={onListToggle}>접기</span>
                          )}
                     </StRecommendHeader>
-                    <StRecommend>
-                         <div>
-                              <span>
-                                   # 등산초보 산린이를 위한 산행 코스 추천
-                              </span>
-                              <img
-                                   alt=""
-                                   src="/icons/등산초보 산린이를 위한 산행 코스 추천.png"
-                              />
-                         </div>
-                         <div>
-                              <span>
-                                   # 2022 마지막 단풍놀이, 이곳에서 즐겨요!
-                              </span>
-                              <img
-                                   alt=""
-                                   src="/icons/2022 마지막 단풍놀이, 이곳에서 즐겨요!.png"
-                              />
-                         </div>
-                         {recommendList === false ? null : (
-                              <>
-                                   <div>
-                                        <span>
-                                             # 가족과 함께 가볍게 즐기기 좋은
-                                             짧은 산행 코스
-                                        </span>
-                                        <img
-                                             alt=""
-                                             src="/icons/가족과 함께 가볍게 즐기기 좋은 짧은 산행 코스.png"
-                                        />
+                    { recommendList === false ? 
+                         <StRecommend>
+                              {data?.data.tagList.slice(0, 2).map((el, idx) => (
+                                        <div key={idx}>
+                                             <span>{el.name}</span>
+                                             <img src={el.img} alt="" />
+                                        </div>
+                                   )
+                              )}
+                         </StRecommend> :
+                         <StRecommend>
+                         {data?.data.tagList.map((el, idx) => (
+                                   <div key={idx}>
+                                        <span>{el.name}</span>
+                                        <img src={el.img} alt="" />
                                    </div>
-                                   <div>
-                                        <span>
-                                             # 서울 도심에서 즐기는 등산 코스
-                                        </span>
-                                        <img
-                                             alt=""
-                                             src="/icons/서울 도심에서 즐기는 등산 코스.png"
-                                        />
-                                   </div>
-                                   <div>
-                                        <span>
-                                             # 이번 주말 데이트로 추천하는
-                                             가벼운 등산 코스
-                                        </span>
-                                        <img
-                                             alt=""
-                                             src="/icons/이번 주말 데이트로 추천하는 가벼운 등산 코스.png"
-                                        />
-                                   </div>
-                                   <div>
-                                        <span>
-                                             # 한걸음 더 나아가, 중급 등산러를
-                                             위한 추천 리스트
-                                        </span>
-                                        <img
-                                             alt=""
-                                             src="/icons/한걸음 더 나아가, 중급 등산러를 위한 추천 리스트.png"
-                                        />
-                                   </div>
-                              </>
+                              )
                          )}
-                    </StRecommend>
+                    </StRecommend> 
+               }
                </StRecommendWrap>
           </>
      );

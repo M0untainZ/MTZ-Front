1ff99d47e03b78ebc4754d05e5a2c2b32c5a857f
OH <rudals2148@naver.com>
2022-12-09 01:29:46 +0900

    🔨Refactor

--- src/components/kakao/OAuth.jsx
+++ src/components/kakao/OAuth.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 const OAuth = () => {
-  const KAKAO_CLIENT_ID = "cc12618ddc81cd5d765c74e98974c11b";
-  const KAKAO_REDIRECT_URL = "http://localhost:3000/oauth/callback/kakao";
+  const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID
+  const KAKAO_REDIRECT_URL = process.env.REACT_APP_KAKAO_REDIRECT_URL
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
 
   return (

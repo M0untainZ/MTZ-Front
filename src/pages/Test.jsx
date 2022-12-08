9046ee6c0c7afac84098f82ac5968bb48aa9e4d7
OH <rudals2148@naver.com>
2022-12-08 15:26:16 +0900

    ➖Remove

--- src/pages/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import ImageUploading from "react-images-uploading";
-import MountTest from "../components/detailTwo/MountTest";
-const Test = () => {
-  return <MountTest />;
-};
-
-export default Test;
